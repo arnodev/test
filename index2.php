5d479ddcc70b944bcc8dee0a086db275a496706f
arnodev <arnaud.teston@gmail.com>
2014-11-15 21:09:01 +0100

    all

---
+++ index2.php
@@ -0,0 +1,21 @@
+<?php
+	phpinfo();
+	echo 'yo !!!';
+	
+	// $link = mysql_connect("192.168.34.10", "root", "test");
+	// if (!$link) {
+	   // die('Impossible de se connecter : ' . mysql_error());
+	// }
+
+	// Rendre la base de données foo, la base courante
+	// $db_selected = mysql_select_db('dev', $link);
+	// if (!$db_selected) {
+	   // die ('Impossible de sélectionner la base de données : ' . mysql_error());
+	// }
+	
+	// $objSql = mysql_query('select * from users');
+	// print_r($objSql);
+	
+	// mysql_close($link);
+	
+?>
---
+++ indexTests.php
@@ -0,0 +1,14 @@
+<?php
+class MainTests extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+    }
+    
+    public function testEspion()
+    {
+		$bResult = true;
+		$this->assertEquals(true, $bResult);
+    }
+}
+?>
