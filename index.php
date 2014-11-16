19816d04f392eebece9bf9d4aad1d2c528ad6982
arnodev <arnaud.teston@gmail.com>
2014-11-16 13:07:29 +0000

    correction

--- index.php
+++ index.php
@@ -4,14 +4,9 @@
 	}
 	
 	phpinfo();
-	
-<<<<<<< HEAD
-	$bResult = false;
-	if ( true && $bResult == false ) {
-=======
+
 	$bResult = true;
 	if ( true && $bResult == true ) {
->>>>>>> 61b70344359d621ae1b9d0119ed0d91c9121de87
 		echo 'yes';
 	}
 	
